cb94b45a63f84a1483c0e14f746a9962b5920f0a
mvl22 <martin@lucas-smith.co.uk>
2013-08-26 12:00:00 +0000

    Version 1.20.6 (released 26/Aug/2013)
    * Implemented `defaultPresplit`, `separator` and `separatorSurround` attributes for `checkboxes`
    * Added a static `getCountries` function containing a list of countries
    * MsoNormal CSS class also now cleared in HTML cleaning routine

--- src/ultimateForm.php
+++ src/ultimateForm.php
@@ -57,7 +57,7 @@
  * @license	http://opensource.org/licenses/gpl-license.php GNU Public License
  * @author	{@link http://www.geog.cam.ac.uk/contacts/webmaster.html Martin Lucas-Smith}, University of Cambridge
  * @copyright Copyright  2003-13, Martin Lucas-Smith, University of Cambridge
- * @version 1.20.4
+ * @version 1.20.6
  */
 class form
 {
@@ -1214,6 +1214,8 @@ class form
 				"<(li|tr|/tr|tbody|/tbody)"	=> "\t<\\1",	// Indent level-two tags
 				"<td"	=> "\t\t<td",	// Double-indent level-three tags
 				'<h([1-6]+) id="Heading([0-9]+)">'      => '<h\\1>',    // Headings from R2Net converter
+				' class="MsoNormal"' => '',	// WordHTML
+				' class="MsoNormal c([0-9]+)"' => '',	// WordHTML
 			);
 		}
 		
@@ -1292,6 +1294,8 @@ class form
 			'autofocus'				=> false,	# HTML5 autofocus (true/false)
 			'default'				=> array (),# Pre-selected item(s)
 			'defaultPresplit'		=> false,	# Whether to pre-split a default that is a string, using the separator and separatorSurround values
+			'separator'				=> ",\n",	# Separator used for the compiled and presented output types
+			'separatorSurround'		=> false,	# Whether, for the compiled and presented output types, if there are any output values, the separator should also be used to surround the values (e.g. |value1|value2|value3| rather than value1|value2|value3 for separator = '|')
 			'forceAssociative'		=> false,	# Force the supplied array of values to be associative
 			'nullText'				=> $this->settings['nullText'],	# Override null text for a specific widget
 			'discard'				=> false,	# Whether to process the input but then discard it in the results
@@ -1304,8 +1308,6 @@ class form
 			'copyTo'				=> false,	# Whether to copy the value, onchange, to another form widget if that widget's value is currently empty
 			'autocomplete'			=> false,	# URL of data provider
 			'autocompleteOptions'	=> false,	# Autocomplete options; see: http://jqueryui.com/demos/autocomplete/#remote (this is the new plugin)
-			'separator'				=> ",\n",	# Separator used for the compiled and presented output types
-			'separatorSurround'		=> false,	# Whether, for the compiled and presented output types, if there are any output values, the separator should also be used to surround the values (e.g. |value1|value2|value3| rather than value1|value2|value3 for separator = '|')
 		);
 		
 		# Create a new form widget
@@ -1327,7 +1329,7 @@ class form
 					}
 				}
 				if ($splittableString) {
-					$arguments['default'] = explode ($arguments['separator'], $matches[1]);
+					$arguments['default'] = explode ($arguments['separator'], $arguments['default']);
 				}
 			}
 		}
@@ -1982,6 +1984,10 @@ class form
 			'maximum'				=> 0,		# The maximum number which must be selected (defaults to 0, i.e. no maximum checking done)
 			'autofocus'				=> false,	# HTML5 autofocus (true/false)
 			'default'			=> array (),	# Pre-selected item(s); if a string is supplied it will be converted to an array of one item
+			'defaultPresplit'		=> false,	# Whether to pre-split a default that is a string, using the separator and separatorSurround values
+			'separator'				=> ",\n",	# Separator used for the compiled and presented output types
+			'separatorSpecialSetdatatype' => ',',	# Separator used for the special-setdatatype output types
+			'separatorSurround'		=> false,	# Whether, for the compiled and presented output types, if there are any output values, the separator should also be used to surround the values (e.g. |value1|value2|value3| rather than value1|value2|value3 for separator = '|')
 			'forceAssociative'		=> false,	# Force the supplied array of values to be associative
 			'linebreaks'			=> $this->settings['linebreaks'],	# Whether to put line-breaks after each widget: true = yes (default) / false = none / array (1,2,5) = line breaks after the 1st, 2nd, 5th items
 			'columns'				=> false,	# Split into columns
@@ -1997,6 +2003,26 @@ class form
 		$widget = new formWidget ($this, $suppliedArguments, $argumentDefaults, __FUNCTION__, NULL, $arrayType = true);
 		
 		$arguments = $widget->getArguments ();
+		
+		# If pre-splitting is required, split
+		#!# Ideally this would use separatorSpecialSetdatatype instead but ultimateForm has no way of being sure which format is being supplied
+		if ($arguments['defaultPresplit']) {
+			if (is_string ($arguments['default']) && strlen ($arguments['default'])) {
+				$splittableString = true;
+				if ($arguments['separatorSurround']) {
+					$splittableString = false;
+					$delimiter = '/';
+					$delimiter . '^' . preg_quote ($arguments['separator']) . '(.+)' . preg_quote ($arguments['separator']) . '$' . $delimiter;
+					if (preg_match ($delimiter . '^' . preg_quote ($arguments['separator'], $delimiter) . '(.+)' . preg_quote ($arguments['separator'], $delimiter) . '$' . $delimiter, $arguments['default'], $matches)) {
+						$splittableString = true;
+						$arguments['default'] = $matches[1];
+					}
+				}
+				if ($splittableString) {
+					$arguments['default'] = explode ($arguments['separator'], $arguments['default']);
+				}
+			}
+		}
 		
 		# Ensure the supplied values and initial value(s) are each an array, even if only an empty one, converting if necessary
 		$arguments['values'] = application::ensureArray ($arguments['values']);
@@ -2177,9 +2203,16 @@ class form
 			}
 			
 			# Assemble the compiled and presented versions
-			$data['compiled'] = implode (",\n", $chosenValues);
-			$data['presented'] = implode (",\n", $chosenVisible);
-			$data['special-setdatatype'] = implode (',', $chosenValues);
+			$data['compiled'] = implode ($arguments['separator'], $chosenValues);
+			$data['presented'] = implode ($arguments['separator'], $chosenVisible);
+			$data['special-setdatatype'] = implode ($arguments['separatorSpecialSetdatatype'], $chosenValues);
+			
+			# Add the surround if required
+			if ($arguments['separatorSurround']) {
+				if (strlen ($data['compiled']))  {$data['compiled']  = $arguments['separator'] . $data['compiled']  . $arguments['separator'];}
+				if (strlen ($data['presented'])) {$data['presented'] = $arguments['separator'] . $data['presented'] . $arguments['separator'];}
+				if (strlen ($data['special-setdatatype'])) {$data['special-setdatatype'] = $arguments['separatorSpecialSetdatatype'] . $data['special-setdatatype'] . $arguments['separatorSpecialSetdatatype'];}
+			}
 		}
 		
 		# Compile the datatype
@@ -7063,6 +7096,271 @@ class form
 			}
 		}
 	}
+	# Function to return a list of countries
+	public static function getCountries ($additionalStart = array ())
+	{
+		# Define the main list
+		$countries = array (
+			'Afghanistan',
+			'Aland Islands',
+			'Albania',
+			'Algeria',
+			'American Samoa',
+			'Andorra',
+			'Angola',
+			'Anguilla',
+			'Antarctica',
+			'Antigua and Barbuda',
+			'Argentina',
+			'Armenia',
+			'Aruba',
+			'Australia',
+			'Austria',
+			'Azerbaijan',
+			'Bahamas',
+			'Bahrain',
+			'Bangladesh',
+			'Barbados',
+			'Belarus',
+			'Belgium',
+			'Belize',
+			'Benin',
+			'Bermuda',
+			'Bhutan',
+			'Bolivia, Plurinational State of',
+			'Bonaire, Saint Eustatius and Saba',
+			'Bosnia and Herzegovina',
+			'Botswana',
+			'Bouvet Island',
+			'Brazil',
+			'British Indian Ocean Territory',
+			'Brunei Darussalam',
+			'Bulgaria',
+			'Burkina Faso',
+			'Burundi',
+			'Cambodia',
+			'Cameroon',
+			'Canada',
+			'Cape Verde',
+			'Cayman Islands',
+			'Central African Republic',
+			'Chad',
+			'Chile',
+			'China',
+			'Christmas Island',
+			'Cocos (Keeling) Islands',
+			'Colombia',
+			'Comoros',
+			'Congo',
+			'Congo, The Democratic Republic of the',
+			'Cook Islands',
+			'Costa Rica',
+			"Cote d'Ivoire",
+			'Croatia',
+			'Cuba',
+			'Curacao',
+			'Cyprus',
+			'Czech Republic',
+			'Denmark',
+			'Djibouti',
+			'Dominica',
+			'Dominican Republic',
+			'Ecuador',
+			'Egypt',
+			'El Salvador',
+			'Equatorial Guinea',
+			'Eritrea',
+			'Estonia',
+			'Ethiopia',
+			'Falkland Islands (Malvinas)',
+			'Faroe Islands',
+			'Fiji',
+			'Finland',
+			'France',
+			'French Guiana',
+			'French Polynesia',
+			'French Southern Territories',
+			'Gabon',
+			'Gambia',
+			'Georgia',
+			'Germany',
+			'Ghana',
+			'Gibraltar',
+			'Greece',
+			'Greenland',
+			'Grenada',
+			'Guadeloupe',
+			'Guam',
+			'Guatemala',
+			'Guernsey',
+			'Guinea',
+			'Guinea-Bissau',
+			'Guyana',
+			'Haiti',
+			'Heard Island and McDonald Islands',
+			'Holy See (Vatican City State)',
+			'Honduras',
+			'Hong Kong',
+			'Hungary',
+			'Iceland',
+			'India',
+			'Indonesia',
+			'Iran, Islamic Republic of',
+			'Iraq',
+			'Ireland',
+			'Isle of Man',
+			'Israel',
+			'Italy',
+			'Jamaica',
+			'Japan',
+			'Jersey',
+			'Jordan',
+			'Kazakhstan',
+			'Kenya',
+			'Kiribati',
+			"Korea, Democratic People's Republic of",
+			'Korea, Republic of',
+			'Kuwait',
+			'Kyrgyzstan',
+			"Lao People's Democratic Republic",
+			'Latvia',
+			'Lebanon',
+			'Lesotho',
+			'Liberia',
+			'Libyan Arab Jamahiriya',
+			'Liechtenstein',
+			'Lithuania',
+			'Luxembourg',
+			'Macao',
+			'Macedonia, The Former Yugoslav Republic of',
+			'Madagascar',
+			'Malawi',
+			'Malaysia',
+			'Maldives',
+			'Mali',
+			'Malta',
+			'Marshall Islands',
+			'Martinique',
+			'Mauritania',
+			'Mauritius',
+			'Mayotte',
+			'Mexico',
+			'Micronesia, Federated States of',
+			'Moldova, Republic of',
+			'Monaco',
+			'Mongolia',
+			'Montenegro',
+			'Montserrat',
+			'Morocco',
+			'Mozambique',
+			'Myanmar',
+			'Namibia',
+			'Nauru',
+			'Nepal',
+			'Netherlands',
+			'New Caledonia',
+			'New Zealand',
+			'Nicaragua',
+			'Niger',
+			'Nigeria',
+			'Niue',
+			'Norfolk Island',
+			'Northern Mariana Islands',
+			'Norway',
+			'Occupied Palestinian Territory',
+			'Oman',
+			'Pakistan',
+			'Palau',
+			'Panama',
+			'Papua New Guinea',
+			'Paraguay',
+			'Peru',
+			'Philippines',
+			'Pitcairn',
+			'Poland',
+			'Portugal',
+			'Puerto Rico',
+			'Qatar',
+			'Reunion',
+			'Romania',
+			'Russian Federation',
+			'Rwanda',
+			'Saint Barthelemy',
+			'Saint Helena, Ascension and Tristan da Cunha',
+			'Saint Kitts and Nevis',
+			'Saint Lucia',
+			'Saint Martin (French part)',
+			'Saint Pierre and Miquelon',
+			'Saint Vincent and The Grenadines',
+			'Samoa',
+			'San Marino',
+			'Sao Tome and Principe',
+			'Saudi Arabia',
+			'Senegal',
+			'Serbia',
+			'Seychelles',
+			'Sierra Leone',
+			'Singapore',
+			'Sint Maarten (Dutch part)',
+			'Slovakia',
+			'Slovenia',
+			'Solomon Islands',
+			'Somalia',
+			'South Africa',
+			'South Georgia and the South Sandwich Islands',
+			'Spain',
+			'Sri Lanka',
+			'North Sudan',
+			'South Sudan',
+			'Suriname',
+			'Svalbard and Jan Mayen',
+			'Swaziland',
+			'Sweden',
+			'Switzerland',
+			'Syrian Arab Republic',
+			'Taiwan, Province of China',
+			'Tajikistan',
+			'Tanzania, United Republic of',
+			'Thailand',
+			'Timor-Leste',
+			'Togo',
+			'Tokelau',
+			'Tonga',
+			'Trinidad and Tobago',
+			'Tunisia',
+			'Turkey',
+			'Turkmenistan',
+			'Turks and Caicos Islands',
+			'Tuvalu',
+			'Uganda',
+			'Ukraine',
+			'United Arab Emirates',
+			'United Kingdom (UK)',
+			'United States of America (USA)',
+			'United States Minor Outlying Islands',
+			'Uruguay',
+			'Uzbekistan',
+			'Vanuatu',
+			'Venezuela, Bolivarian Republic of',
+			'Viet Nam',
+			'Virgin Islands, British',
+			'Virgin Islands, U.S.',
+			'Wallis and Futuna',
+			'Western Sahara',
+			'Yemen',
+			'Zambia',
+			'Zimbabwe',
+		);
+		
+		# Add any additional to the start
+		if ($additionalStart) {
+			$additionalStart[] = '---';
+			$countries = array_merge ($additionalStart, $countries);
+		}
+		
+		# Return the list
+		return $countries;
+	}
 }
 
 
@@ -7615,5 +7913,10 @@ class formWidget
 #!# 	Style like in http://www.sitepoint.com/examples/simpletricks/form-demo.html [linked from http://www.sitepoint.com/article/1273/3]
 #!# Add AJAX validation flag See: http://particletree.com/features/degradable-ajax-form-validation/ (but modified version needed because this doesn't use Unobtrusive DHTML - see also http://particletree.com/features/a-guide-to-unobtrusive-javascript-validation/ )
 
+
+# Restructuring suggestion notes:
+# Widgets shouldn't make a new array at the end, since this mostly copies existing data. Instead enable the settings to be looked up, with only genuinely-new output stuff added
+# Widget parameters are of two types: generic (e.g. 'discard') and specific. So the generic stuff should be migrated out of the per-widget handling.
+# The $elementValue = $widget->getValue (); passing is confusing
 
 ?>
